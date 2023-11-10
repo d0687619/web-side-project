7f97db8eb4ef79803fba2b9fc645696ea7bb17a0
jeffy <jeffyang871217@gmail.com>
2023-11-11 00:14:56 +0800

    player.html

--- main.js
+++ main.js
@@ -18,6 +18,15 @@ $( function() {
   $( "#tabs" ).tabs();
 } );
 
+$( function() {
+  $( "#tabs" ).tabs({
+    event: "mouseover"
+  });
+  $( "#tabsb" ).tabs({
+    event: "mouseover"
+  });
+} );
+
 $(function() {
   $(".tabJoined").tabs({
     activate: function(e, ui) {
@@ -30,14 +39,3 @@ $(function() {
   });
 });
 
-
-var line = $('#line');
-var div1 = $('#one');
-var div2 = $('#two');
-
-var x1 = div1.offset().left + (div1.width()/2);
-var y1 = div1.offset().top + (div1.height()/2);
-var x2 = div2.offset().left + (div2.width()/2);
-var y2 = div2.offset().top + (div2.height()/2);
-
-line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
